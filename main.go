57acc58725ea1071c9b0dcaad170f0f1214ec8ba
Harsh Dobariya <harshdobariya79@gmail.com>
2024-08-20 22:20:58 +0530

    Initialize go module

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Starting server on port 8080")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Server error:", err)
+	}
+}
